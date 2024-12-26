f0aae184dbf312740a54f368edc559c815e6514b
Jiyeol Lee <ka.felix496@gmail.com>
2024-12-26 12:32:45 -0500

    update obsidian configuration

--- .nvim/lua/custom/obsidian.lua
+++ .nvim/lua/custom/obsidian.lua
@@ -37,6 +37,9 @@ M.config = function()
     --  * "notes_subdir" - put new notes in the default notes subdirectory.
     new_notes_location = "notes_subdir",
 
+    -- Either 'wiki' or 'markdown'.
+    preferred_link_style = "markdown",
+
     -- Optional, alternatively you can customize the frontmatter data.
     ---@return table
     note_frontmatter_func = function(note)
@@ -111,6 +114,60 @@ M.config = function()
       -- vim.cmd(':silent exec "!start ' .. url .. '"') -- Windows
     end,
 
+    -- Optional, set to true to force ':ObsidianOpen' to bring the app to the foreground.
+    open_app_foreground = true,
+
+    picker = {
+      -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
+      name = "telescope.nvim",
+      -- Optional, configure key mappings for the picker. These are the defaults.
+      -- Not all pickers support all mappings.
+      note_mappings = {
+        -- Create a new note from your query.
+        new = "<C-x>",
+        -- Insert a link to the selected note.
+        insert_link = "<C-l>",
+      },
+      tag_mappings = {
+        -- Add tag(s) to current note.
+        tag_note = "<C-x>",
+        -- Insert a tag at the current location.
+        insert_tag = "<C-l>",
+      },
+    },
+
+    workspaces = {
+      {
+        name = "personal",
+        path = "~/vaults/vault-personal",
+      },
+      {
+        name = "work-2",
+        path = "~/vaults/vault-work-2",
+      },
+      {
+        name = "work-3",
+        path = "~/vaults/vault-work-3",
+      },
+    },
+
+    mappings = {
+      -- Toggle check-boxes.
+      ["<leader>ch"] = {
+        action = function()
+          return require("obsidian").util.toggle_checkbox()
+        end,
+        opts = { buffer = true },
+      },
+      -- Smart action depending on context, either follow link or toggle checkbox.
+      ["<cr>"] = {
+        action = function()
+          return require("obsidian").util.smart_action()
+        end,
+        opts = { buffer = true, expr = true },
+      },
+    },
+
     -- Specify how to handle attachments.
     attachments = {
       -- The default folder to place images in via `:ObsidianPasteImg`.
@@ -137,62 +194,6 @@ M.config = function()
       end,
     },
 
-    workspaces = {
-      {
-        name = "personal",
-        path = "~/vaults/vault-personal",
-      },
-      {
-        name = "work-2",
-        path = "~/vaults/vault-work-2",
-      },
-      {
-        name = "work-3",
-        path = "~/vaults/vault-work-3",
-      },
-    },
-
-    mappings = {
-      -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
-      ["gf"] = {
-        action = function()
-          return require("obsidian").util.gf_passthrough()
-        end,
-        opts = { noremap = false, expr = true, buffer = true },
-      },
-      -- Toggle check-boxes.
-      ["<leader>ch"] = {
-        action = function()
-          return require("obsidian").util.toggle_checkbox()
-        end,
-        opts = { buffer = true },
-      },
-      -- Smart action depending on context, either follow link or toggle checkbox.
-      ["<cr>"] = {
-        action = function()
-          return require("obsidian").util.smart_action()
-        end,
-        opts = { buffer = true, expr = true },
-      },
-      ["<leader>oo"] = {
-        action = function()
-          local obsidian = require("obsidian")
-          local client = obsidian.get_client()
-          local Workspace = require("obsidian.Workspace")
-          local options = {}
-          for i, spec in ipairs(client.opts.workspaces) do
-            local workspace = Workspace.new_from_spec(spec)
-            if workspace == client.current_workspace then
-              options[#options + 1] = string.format("*[%d] %s @ '%s'", i, workspace.name, workspace.path)
-            else
-              options[#options + 1] = string.format("[%d] %s @ '%s'", i, workspace.name, workspace.path)
-            end
-          end
-          return nil
-        end,
-        opts = { buffer = true, expr = true },
-      }
-    },
   }
 
   obsidian.setup(setup)
