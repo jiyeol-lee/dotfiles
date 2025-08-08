8d482a03ed507622f91829041e170b61d501feba
Jiyeol Lee <jiyeol.tech@gmail.com>
2025-08-08 16:34:52 -0400

    chore: create config files and vault directories
    create a shell script and run in bootstrap.sh and also when the shell is
    initialized as well

---
+++ create_config_folders.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Create config directories
+mkdir -p ~/.config/alacritty
+mkdir -p ~/.config/tmux
+mkdir -p ~/.docker
+mkdir -p ~/.ssh
+
+# Create vault directories
+mkdir -p ~/vaults/vpersonal
+mkdir -p ~/vaults/vwork2
+mkdir -p ~/vaults/vwork3
