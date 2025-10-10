5b1d822feffd709afdc5bd4e4cedb6231cf631fc
Jiyeol Lee <jiyeol.tech@gmail.com>
2025-10-10 10:11:42 -0400

    chore(code-companion): use gpt5 as default
    claude-sonnet-4.5 is very much buggy with code companion

--- .nvim/lua/plugins/code-companion/code-companion.lua
+++ .nvim/lua/plugins/code-companion/code-companion.lua
@@ -50,8 +50,8 @@ function M.config()
           return require("codecompanion.adapters").extend("copilot", {
             schema = {
               model = {
-                default = "claude-sonnet-4.5",
-                -- default = "gpt-5",
+                -- default = "claude-sonnet-4.5",
+                default = "gpt-5",
                 -- default = "gemini-2.5-pro",
               },
             },
