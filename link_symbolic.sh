8e6a5759de84fd51691cbb41813cf473550b092a
Jiyeol Lee <jiyeol.tech@gmail.com>
2025-10-16 17:51:19 -0400

    chore(code-companion): symbolic link mcphub servers config

--- link_symbolic.sh
+++ link_symbolic.sh
@@ -4,6 +4,7 @@ ln -snf ~/.config/containers/auth.json ~/.config/containers/config.json
 ln -snf ~/dotfiles/.tmux.conf ~/.config/tmux/tmux.conf
 ln -snf ~/dotfiles/.activated_gh_account.sh ~/.config/tmux/.__activated_gh_account.sh
 ln -snf ~/dotfiles/.alacritty.toml ~/.config/alacritty/alacritty.toml
+ln -snf ~/dotfiles/.mcphub.json ~/.config/mcphub/servers.json
 case "$(uname -s)" in
 Linux*) ln -snf ~/dotfiles/.alacritty-bindings-nonmacos.toml ~/.config/alacritty/alacritty-bindings.toml ;;
 Darwin*) ln -snf ~/dotfiles/.alacritty-bindings-macos.toml ~/.config/alacritty/alacritty-bindings.toml ;;
