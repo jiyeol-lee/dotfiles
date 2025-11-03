e1276403a7d1e807652c092f8f4abdcaf260b92b
Jiyeol Lee <41712713+jiyeol-lee@users.noreply.github.com>
2025-11-03 11:08:46 -0500

    chore(scripts): github pr list with graphql

--- scripts/github_prs.sh
+++ scripts/github_prs.sh
@@ -1,22 +1,66 @@
 #!/bin/bash
 
-TEMPLATE_PR_LIST=$(awk '{ gsub(/  /, ""); printf "%s", $0 }' <~/dotfiles/templates/gh_pr_list.gotmpl)
-TEMPLATE_SEARCH_PRS=$(awk '{ gsub(/  /, ""); printf "%s", $0 }' <~/dotfiles/templates/gh_search_prs.gotmpl)
+gh api graphql -f query='
+  query($q:String!) {
+    search(query:$q, type:ISSUE, last:50) {
+      nodes {
+        ... on PullRequest {
+          number
+          title
+          url
+          reviewDecision
+          bodyText
+          repository { name owner { login } }
 
-echo -e "\n== List of open PRs in the current repository ==\n"
-gh pr list --state open --json number,title,reviewDecision,url,state,author,updatedAt --template "$TEMPLATE_PR_LIST"
+          comments(last:50) {
+            nodes {
+              author {
+                login
+                ... on User { name }
+                ... on Organization { name }
+              }
+              bodyText
+              url
+              createdAt
+            }
+          }
 
-echo -e "\n== List of open PRs in all repositories that I have created ==\n"
-gh search prs --state open --author @me --sort updated --json id,repository,number,title,url,state,author,updatedAt --template "$TEMPLATE_SEARCH_PRS"
+          reviews(last:50) {
+            nodes {
+              author {
+                login
+                ... on User { name }
+                ... on Organization { name }
+              }
+              bodyText
+              state
+              url
+              submittedAt
+            }
+          }
 
-echo -e "\n== List of open PRs in all repositories that I have created and approved ==\n"
-gh search prs --state open --review approved --author @me --sort updated --json id,repository,number,title,url,state,author,updatedAt --template "$TEMPLATE_SEARCH_PRS"
-
-echo -e "\n== List of open PRs in all repositories that I have created and required review on ==\n"
-gh search prs --state open --review required --author @me --sort updated --json id,repository,number,title,url,state,author,updatedAt --template "$TEMPLATE_SEARCH_PRS"
-
-echo -e "\n== List of open PRs in all repositories that I have created and requested changes on ==\n"
-gh search prs --state open --review changes_requested --author @me --sort updated --json id,repository,number,title,url,state,author,updatedAt --template "$TEMPLATE_SEARCH_PRS"
-
-echo -e "\n== List of open PRs in all repositories that I have requested review on ==\n"
-gh search prs --state open --review-requested @me --sort updated --json repository,number,title,url,state,author,updatedAt --template "$TEMPLATE_SEARCH_PRS"
+          reviewThreads(last:50) {
+            nodes {
+              isResolved
+              comments(last:50) {
+                nodes {
+                  author {
+                    login
+                    ... on User { name }
+                    ... on Organization { name }
+                  }
+                  bodyText
+                  url
+                  createdAt
+                  path
+                  outdated
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }' \
+  -F q="is:pr is:open author:$(gh api user -q .login) sort:updated-desc" \
+  --template "$(cat ~/dotfiles/templates/gh_pr_list.gotmpl)"
