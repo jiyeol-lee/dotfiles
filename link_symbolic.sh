98ea19585b68ddb4417b25470ef8a75585d4417d
Jiyeol Lee <jiyeol.tech@gmail.com>
2025-11-10 14:54:24 -0500

    chore(symbolic-link): create folder before link

--- link_symbolic.sh
+++ link_symbolic.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
+mkdir -p ~/.config/containers
 ln -snf ~/.config/containers/auth.json ~/.config/containers/config.json
+
+mkdir -p ~/.config/tmux
 ln -snf ~/dotfiles/.tmux.conf ~/.config/tmux/tmux.conf
 ln -snf ~/dotfiles/.activated_gh_account.sh ~/.config/tmux/.__activated_gh_account.sh
+
+mkdir -p ~/.config/alacritty
 ln -snf ~/dotfiles/.alacritty.toml ~/.config/alacritty/alacritty.toml
+
+mkdir -p ~/.config/mcphub
 ln -snf ~/dotfiles/.mcphub.json ~/.config/mcphub/servers.json
 case "$(uname -s)" in
 Linux*) ln -snf ~/dotfiles/.alacritty-bindings-nonmacos.toml ~/.config/alacritty/alacritty-bindings.toml ;;
 Darwin*) ln -snf ~/dotfiles/.alacritty-bindings-macos.toml ~/.config/alacritty/alacritty-bindings.toml ;;
 esac
 
+mkdir -p ~/.config/nvim
 ln -snf ~/dotfiles/.nvim ~/.config/nvim
-ln -snf ~/dotfiles/.__editorconfig ~/.editorconfig
+
+mkdir -p ~/.ssh
 ln -snf ~/dotfiles/.ssh_config ~/.ssh/config
+
+ln -snf ~/dotfiles/.__editorconfig ~/.editorconfig
