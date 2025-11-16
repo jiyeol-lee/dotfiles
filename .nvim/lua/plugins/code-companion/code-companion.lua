35dea2d4628a3adb7a3a624cb60b7da87bc5cab7
Jiyeol Lee <jiyeol.tech@gmail.com>
2025-11-16 18:22:21 -0500

    chore(mcphub): remove mcphub
    i do not need it in code companion. i use this only for very simple
    task, no need for mcp server

--- .nvim/lua/plugins/code-companion/code-companion.lua
+++ .nvim/lua/plugins/code-companion/code-companion.lua
@@ -4,7 +4,6 @@ local M = {
   version = "*",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "ravitemer/mcphub.nvim",
   },
 }
 
@@ -49,22 +48,6 @@ function M.config()
             },
           })
         end,
-      },
-    },
-    extensions = {
-      mcphub = {
-        callback = "mcphub.extensions.codecompanion",
-        opts = {
-          -- MCP Tools
-          make_tools = true,                   -- Make individual tools (@server__tool) and server groups (@server) from MCP servers
-          show_server_tools_in_chat = true,    -- Show individual tools in chat completion (when make_tools=true)
-          add_mcp_prefix_to_tool_names = true, -- Add mcp__ prefix (e.g `@mcp__github`, `@mcp__neovim__list_issues`)
-          show_result_in_chat = false,         -- Show tool results directly in chat buffer
-          -- MCP Resources
-          make_vars = true,                    -- Convert MCP resources to #variables for prompts
-          -- MCP Prompts
-          make_slash_commands = false,         -- Add MCP prompts as /slash commands
-        },
       },
     },
     display = {
