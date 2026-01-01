fce1529b609f401e0088a9969d2f9814e9c28aaf
Jiyeol Lee <41712713+jiyeol-lee@users.noreply.github.com>
2026-01-01 14:36:46 -0500

    chore(git-config): do not expose email anymore

--- scripts/git_config.sh
+++ scripts/git_config.sh
@@ -1,4 +1,4 @@
 git config --global user.name "Jiyeol Lee"
-git config --global user.email "jiyeol.tech@gmail.com"
+git config --global user.email "41712713+jiyeol-lee@users.noreply.github.com"
 git config --global rerere.enabled true # Enable rerere to make my life easier
 git config --global pull.rebase true    # Use rebase when pulling
