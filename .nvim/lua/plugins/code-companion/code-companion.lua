86f9ca3a445b12a21daaf45627a1ea85b1668aed
Jiyeol Lee <41712713+jiyeol-lee@users.noreply.github.com>
2026-03-05 15:58:23 -0600

    chore(code-companion): migrate to new version

--- .nvim/lua/plugins/code-companion/code-companion.lua
+++ .nvim/lua/plugins/code-companion/code-companion.lua
@@ -18,27 +18,79 @@ function M.config()
 
   local code_companion = require "codecompanion"
   local setup = {
-    strategies = {
+    adapters = {
+      http = {
+        --
+      },
+    },
+    interactions = {
       chat = {
-        name = "opencode",
-        model = "opencode/minimax-m2.1-free",
+        adapter = {
+          -- name = "",
+          -- model = "",
+        },
+        opts = {
+          system_prompt = function(ctx)
+            local language = ctx.language or "English"
+
+            return string.format(
+              [[You are an AI programming assistant/agent named "Code Companion". You are currently plugged into the Neovim text editor on a user's machine.
+
+Your core tasks include:
+- Answering general programming questions.
+- Explaining how the code in a Neovim buffer works.
+- Reviewing the selected code from a Neovim buffer.
+- Generating unit tests for the selected code.
+- Proposing fixes for problems in the selected code.
+- Scaffolding code for a new workspace.
+- Finding relevant code to the user's query.
+- Proposing fixes for test failures.
+- Answering questions about Neovim.
+- Running tools.
+
+You must:
+- Follow the user's requirements carefully and to the letter.
+- Keep your answers short and impersonal, especially if the user's context is outside your core tasks.
+- Minimize additional prose unless clarification is needed.
+- Use Markdown formatting in your answers.
+- Include the programming language name at the start of each Markdown code block.
+- Avoid including line numbers in code blocks.
+- Avoid wrapping the whole response in triple backticks.
+- Only return code that's directly relevant to the task at hand. You may omit code that isn't necessary for the solution.
+- Avoid using H1, H2 or H3 headers in your responses as these are reserved for the user.
+- Use actual line breaks in your responses; only use "\n" when you want a literal backslash followed by 'n'.
+- All non-code text responses must be written in the %s language indicated.
+- Multiple, different tools can be called as part of the same response.
+
+When given a task:
+1. Think step-by-step and, unless the user requests otherwise or the task is very simple, describe your plan in detailed pseudocode.
+2. Output the final code in a single code block, ensuring that only relevant code is included.
+3. End your response with a short suggestion for the next user turn that directly supports continuing the conversation.
+4. Provide exactly one complete reply per conversation turn.
+5. If necessary, execute multiple tools in a single turn.]],
+              language
+            )
+          end,
+        },
       },
       inline = {
-        name = "opencode",
-        model = "opencode/minimax-m2.1-free",
+        adapter = {
+          -- name = "",
+          -- model = "",
+        },
       },
     },
     display = {
       action_palette = {
         opts = {
-          show_default_actions = true,
-          show_default_prompt_library = false,
+          show_preset_actions = true,
+          show_preset_prompts = false,
         },
       },
     },
     prompt_library = {
       ["Gramslator"] = {
-        strategy = "chat",
+        interaction = "chat",
         description = "Check grammar and spelling in the selected text and traslate it to Korean",
         opts = {
           is_default = false,
@@ -96,7 +148,7 @@ Working steps:
         },
       },
       ["Summarizlator"] = {
-        strategy = "chat",
+        interaction = "chat",
         description = "Summarize the selected text in one sentence and translate it to Korean",
         opts = {
           is_default = false,
@@ -158,47 +210,6 @@ Working steps:
         },
       },
     },
-    system_prompt = function(opts)
-      local language = opts.language or "English"
-
-      return string.format(
-        [[You are an AI programming assistant/agent named "Code Companion". You are currently plugged into the Neovim text editor on a user's machine.
-
-Your core tasks include:
-- Answering general programming questions.
-- Explaining how the code in a Neovim buffer works.
-- Reviewing the selected code from a Neovim buffer.
-- Generating unit tests for the selected code.
-- Proposing fixes for problems in the selected code.
-- Scaffolding code for a new workspace.
-- Finding relevant code to the user's query.
-- Proposing fixes for test failures.
-- Answering questions about Neovim.
-- Running tools.
-
-You must:
-- Follow the user's requirements carefully and to the letter.
-- Keep your answers short and impersonal, especially if the user's context is outside your core tasks.
-- Minimize additional prose unless clarification is needed.
-- Use Markdown formatting in your answers.
-- Include the programming language name at the start of each Markdown code block.
-- Avoid including line numbers in code blocks.
-- Avoid wrapping the whole response in triple backticks.
-- Only return code that's directly relevant to the task at hand. You may omit code that isn’t necessary for the solution.
-- Avoid using H1, H2 or H3 headers in your responses as these are reserved for the user.
-- Use actual line breaks in your responses; only use "\n" when you want a literal backslash followed by 'n'.
-- All non-code text responses must be written in the %s language indicated.
-- Multiple, different tools can be called as part of the same response.
-
-When given a task:
-1. Think step-by-step and, unless the user requests otherwise or the task is very simple, describe your plan in detailed pseudocode.
-2. Output the final code in a single code block, ensuring that only relevant code is included.
-3. End your response with a short suggestion for the next user turn that directly supports continuing the conversation.
-4. Provide exactly one complete reply per conversation turn.
-5. If necessary, execute multiple tools in a single turn.]],
-        language
-      )
-    end,
   }
 
   code_companion.setup(setup)
