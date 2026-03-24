633c8fba3c22d36204309d54008c891db0152c25
Jiyeol Lee <41712713+jiyeol-lee@users.noreply.github.com>
2026-03-24 11:29:13 -0500

    chore(gitsigns): use default gitsigns colors

--- .nvim/lua/plugins/gitsigns/gitsigns.lua
+++ .nvim/lua/plugins/gitsigns/gitsigns.lua
@@ -45,22 +45,6 @@ M.config = function()
   }
 
   gitsigns.setup(setup)
-
-  vim.api.nvim_set_hl(0, "GitSignsAdd", { link = "DiffAdd" })
-  vim.api.nvim_set_hl(0, "GitSignsChange", { link = "DiffChange" })
-  vim.api.nvim_set_hl(0, "GitSignsDelete", { link = "DiffDelete" })
-  vim.api.nvim_set_hl(0, "GitSignsChangedelete", { link = "DiffChange" })
-  vim.api.nvim_set_hl(0, "GitSignsTopdelete", { link = "DiffDelete" })
-  vim.api.nvim_set_hl(0, "GitSignsAddLn", { link = "DiffAdd" })
-  vim.api.nvim_set_hl(0, "GitSignsChangeLn", { link = "DiffChange" })
-  vim.api.nvim_set_hl(0, "GitSignsDeleteLn", { link = "DiffDelete" })
-  vim.api.nvim_set_hl(0, "GitSignsChangedeleteLn", { link = "DiffChange" })
-  vim.api.nvim_set_hl(0, "GitSignsTopdeleteLn", { link = "DiffDelete" })
-  vim.api.nvim_set_hl(0, "GitSignsAddNr", { link = "DiffAdd" })
-  vim.api.nvim_set_hl(0, "GitSignsChangeNr", { link = "DiffChange" })
-  vim.api.nvim_set_hl(0, "GitSignsDeleteNr", { link = "DiffDelete" })
-  vim.api.nvim_set_hl(0, "GitSignsChangedeleteNr", { link = "DiffChange" })
-  vim.api.nvim_set_hl(0, "GitSignsTopdeleteNr", { link = "DiffDelete" })
 end
 
 return M
